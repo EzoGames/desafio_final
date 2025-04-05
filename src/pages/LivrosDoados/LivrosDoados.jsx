130a0d0715978a2de392098da6bf4fbc70452843
EzoGames <enzoleviaraujo106@gmail.com>
2025-04-05 00:04:49 -0300

    Desafio_Final

--- src/pages/LivrosDoados/LivrosDoados.jsx
+++ src/pages/LivrosDoados/LivrosDoados.jsx
@@ -1,8 +1,42 @@
 import s from './LivrosDoados.module.scss'
 import red from '../../assets/livrored.png'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+
 export default function livrosDoados(){
+    
+    const [livros,setLivros] = useState([])
+
+    const puxarLivros = async() =>{
+            const resposta = await axios.get ("https://desafio-apii1.onrender.com/livros")
+            setLivros(resposta.data)
+    }
+    
+    useEffect (()=>{
+        /*alert("useEffect executou")*/
+        puxarLivros()
+    },[])
+
     return(
+        
         <section className={s.livrosDoados}>
+            <h1 className={s.livroh1}>Livros Doados</h1>
+           <section className={s.containerlivros}>
+           {livros.map((item)=>(
+                <section className={s.Livrosregistrados}>
+                    <img src={item.image_url} alt="" />
+                    <h3>{item.titulo}</h3>
+                    <p>{item.autor}</p>
+                    <p>{item.categoria}</p>
+                </section>
+            ))}
+            </section> 
+        </section>
+        
+        
+           
+      /*  <section className={s.livrosDoados}>
             <h1 className={s.livroh1}>Livros Doados</h1>
            <section className={s.containerlivros}><section className={s.imagem}>
                 <img className={s.imagu} src={red} alt="" />
@@ -13,6 +47,6 @@ export default function livrosDoados(){
                 </section>
             </section></section> 
         </section>
-        
+        */
     )
 }
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,23 +1,55 @@
 import s from './QueroDoar.module.scss'
 import Vector from '../../assets/Vector.png'
+import axios from 'axios'
+import { useState } from 'react'
 export default function queroDoar(){
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [image_url, setImage_url] = useState("")
+
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+    }
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+    const capturaImage_url = (e) => {
+        setImage_url(e.target.value)
+    }
+
+    const enviarDados = async() => {
+        const dadosPEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+
+        await axios.post("https://desafio-apii1.onrender.com/doar", dadosPEnviar)
+    }
+
     return(
         <section className={s.Doar}>
             <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-            <section className={s.cardinho}>
+            <section className={s.cardinho}onSubmit={(e) => e.preventDefault()}>
                 <section className={s.imager}>
                 <img src={Vector} alt="" /> <h3>Informações do Livro</h3>
                 </section>
 
                     <section className={s.dcard}>
                      
-                        <input className={s.inputy} type="text" placeholder='Titulo' />
-                        <input className={s.inputy} type="text" placeholder='Categoria'/>
-                        <input className={s.inputy} type="text" placeholder='Autor'/>
-                        <input className={s.inputy} type="text" placeholder='Link da Imagem'/>
-            
+                        <input className={s.inputy} type="text" placeholder='Titulo' onChange={capturaTitulo}/>
+                        <input className={s.inputy} type="text" placeholder='Categoria' onChange={capturaCategoria}/>
+                        <input className={s.inputy} type="text" placeholder='Autor' onChange={capturaAutor}/>
+                        <input className={s.inputy} type="text" placeholder='Link da Imagem' onChange={capturaImage_url}/>
+
                         <section className={s.vazio}></section>
-                        <button className={s.butau}>Doar</button>
+                        <button onClick={enviarDados} className={s.butau}>Doar</button>
                     </section>
             </section>
             
